bbe0a813fe0d3736d64158bbd8c0b5ae656eb32b
rahulvenugopal <rhlvenugopal@gmail.com>
2022-12-05 08:19:18 +0530

    Update aoc_2021_day_5.py

--- Day5/aoc_2021_day_5.py
+++ Day5/aoc_2021_day_5.py
@@ -48,3 +48,68 @@ print("Points with atleast two lines cross is " + str(counter))
 
 #%% --- Day 5: Hydrothermal Venture --- Part 2
 
+# taking on-diagonal and off diagonal vents too
+
+# create a square array lists with all zeros
+
+import numpy as np
+grid_of_vents = np.zeros((999,999))
+
+for indices,entries in enumerate(data)  :
+    print(indices)
+    x1,y1 = entries.split('->')[0].rstrip().lstrip().split(',')
+    x2,y2 = entries.split('->')[1].rstrip().lstrip().split(',')
+
+    if x1 == x2 and y1 != y2:
+        to_fill = list(range(min(int(y1), int(y2)), max(int(y1), int(y2))+1))
+
+        # add 1 to indicate vent locations
+        for entries in to_fill:
+            grid_of_vents[entries][int(x1)] = grid_of_vents[entries][int(x1)] + 1
+
+    if y1 == y2 and x1 != x2:
+        to_fill = list(range(min(int(x1), int(x2)), max(int(x1), int(x2))+1))
+
+        # add 1 to indicate vent locations
+        for entries in to_fill:
+            grid_of_vents[int(y1)][entries] = grid_of_vents[int(y1)][entries] + 1
+
+    if x1 ==x2 and y1 == y2:
+        to_fill = list(range(min(int(x1), int(y1)), max(int(x2), int(y2))+1))
+
+        # add 1 to indicate vent locations
+        for entries in to_fill:
+            grid_of_vents[entries][entries] = grid_of_vents[entries][entries] + 1
+
+    if abs(int(x1) + int(x2)) == abs(int(y1)-int(y2)):
+        to_fill_xs = list(range((min(int(x1), int(x2))), max(int(x1), int(x2)) +1))
+        to_fill_ys = list(reversed(to_fill_xs))
+
+        # add 1 to indicate vent locations
+        for indices in range(len(to_fill_xs)):
+            grid_of_vents[to_fill_xs[indices]][to_fill_ys[indices]] = grid_of_vents[to_fill_xs[indices]][to_fill_ys[indices]] + 1
+
+
+# count indices in grid which are greater than 2
+counter = 0
+
+for rows in range(len(grid_of_vents)):
+    for columns in range(len(grid_of_vents)):
+        if grid_of_vents[rows][columns] >1:
+            counter += 1
+
+print("Points with atleast two lines cross is " + str(counter))
+
+#%%
+
+data = [
+        '0,9 -> 5,9',
+        '8,0 -> 0,8',
+        '9,4 -> 3,4',
+        '2,2 -> 2,1',
+        '7,0 -> 7,4',
+        '6,4 -> 2,0',
+        '0,9 -> 2,9',
+        '3,4 -> 1,4',
+        '0,0 -> 8,8',
+        '5,5 -> 8,2']
